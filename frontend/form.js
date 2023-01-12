fb5116a181407df74632793993e548bf15d86a89
Owen George <ogeorge5@my.bcit.ca>
2023-01-12 13:22:20 -0800

    test4

--- frontend/form.js
+++ frontend/form.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     const message = $('#chatbot-input').val();
     $.ajax({
       type: 'POST',
-      url: "/chatbot",
+      url: "https://ogeorge-comp4537-lab2.onrender.com//chatbot",
       data: {
         message: message
       },
